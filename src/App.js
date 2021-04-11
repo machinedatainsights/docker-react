960c3e95431ad64a895a3d4ed083a14ee4fdc363
Jim_Baxter <jim.baxter@machinedatainsights.com>
2021-04-11 16:09:49 -0400

    changed app.js text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
             Edit <code>src/App.js</code> and save to reload.
             <br />
-            And.... we changed it again and again :-)!
+            I was changed on the feature branch
         </p>
         <a
           className="App-link"
